296ee2607db0e10bd3a0b253ec51ab84ce75cb6d
vellons <vellons@users.noreply.github.com>
2022-05-17 15:29:20 +0200

    Added username validator

--- src/users/models.py
+++ src/users/models.py
@@ -1,4 +1,7 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
+
+from users.validators import MyUsernameValidator
 
 PROFILE_TYPE_CHOICES = [
     ('STD', 'Standard'),
@@ -7,7 +10,17 @@ PROFILE_TYPE_CHOICES = [
 
 
 class Profile(models.Model):
-    username = models.CharField(max_length=64, unique=True)
+    username = models.CharField(
+        max_length=32,
+        unique=True,
+        help_text=(
+            'Required. 32 characters or fewer. Lowercase letters, digits _ only; must start with a letter.'
+        ),
+        validators=[MinLengthValidator(4), MyUsernameValidator()],
+        error_messages={
+            'unique': 'A user with that username already exists.',
+        },
+    )
     name = models.CharField(max_length=64, null=True)
     surname = models.CharField(max_length=64, null=True)
     type = models.CharField(max_length=3, choices=PROFILE_TYPE_CHOICES, default='STD')
